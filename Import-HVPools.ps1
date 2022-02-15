bf92237ba5c19af4ebbac95272db1696333ea4c0
KByrne9000 <99512733+KByrne9000@users.noreply.github.com>
2022-02-15 11:12:06 -0500

    Add files via upload

---
+++ Import-HVPools.ps1
@@ -0,0 +1,70 @@
+﻿<#
+.SYNOPSIS
+    Import-HVPools.ps1
+.VERSION
+    .8b
+.DESCRIPTION
+    Imports all the pools from a JSON File, adds them to an existing Horizon Connection Server, and entitles those pools from a TXT file.
+.NOTES
+    Author(s): Kevin Byrne
+    Requirements:  
+    Make sure the VMware.HV.Helper module is installed, see: https://github.com/vmware/PowerCLI-Example-Scripts
+    Copy the VMware.Hv.Helper to the module location.
+#>
+
+#Login variables
+###$horizonServer = Read-Host -Prompt "Enter the Horizon Connection Server Name"
+###$username = Read-Host -Prompt "Enter the Username (without the domain name)"
+$password = Read-Host -Prompt "Enter the Password" -AsSecureString
+###$domain = Read-Host -Prompt "Enter the Horizon AD Domain"
+
+# DEBUG login variables
+$horizonServer = "vcs.titleonecorp.com"
+$username = "byrnekevx"
+$domain = "NRTShared"
+
+#File location variable
+$fileloc = "C:\Powershell-Logs\HorizonDB"
+$logfilePath = "C:\Powershell-Logs\HV-Import.txt"
+
+Start-Transcript -Path "$logfilePath" -Append
+
+#Connect to the Horizon Environment
+Write-Output "", "Connect to the Connection Server" 
+Connect-HVServer -Server $horizonServer -Domain $domain -user $username -Password $password
+
+#Importing each pools configuration from individual JSON files, and the Entitlements from individual TXT files
+Write-Output "", "Connection Server pool import!", ""
+# DEBUG For Validation using a Fixed Valiable
+$Pools = "Test-PSScript"
+# For Production use
+###$Poolraw = get-childitem -Path "$fileloc" | where { $_.Name -like "*.json" }
+###                foreach ($Pool in $Poolraw) {                             
+###                $Pools = $Poolraw.name.replace(".json","")
+###           }
+Write-Output "Importing these pools to the Horizon Server: ", $Pools
+
+
+ForEach ($Pool in $Pools) {
+    Write-Output "", "============================================="
+    #Importing the Pool from a JSON file
+    Write-Output "", "Import pool: $Pool", ""
+    
+            if ((Get-HVPool $Pool -SuppressInfo:$true).base.name -ne $Pool) {
+                New-HVPool -PoolName $Pool -Spec $fileloc\$Pool.json -Confirm:$false | Out-Null
+                Start-Sleep -Seconds 5
+                #Importing the Entitlments from a TXT file
+                $Entitlements = Get-Content "$fileloc\$Pool.txt"
+                Write-Output "Adding Entitlements: $Entitlements", "" 
+                ForEach ($Entitlement in $Entitlements) {
+                    New-HVEntitlement -User $Entitlement -ResourceName $Pool
+                    }
+                }
+            else {
+                Write-Output "Pool already exists!"                              
+                }
+    }
+
+Write-Output "", "", "************** Import Complete! **************", ""
+Disconnect-HVServer -Server $horizonServer -Confirm:$false
+Stop-Transcript | Out-Null
---
+++ Import-VMX.ps1
@@ -0,0 +1,89 @@
+﻿<#
+.SYNOPSIS
+    Import-VMX.ps1
+.VERSION
+    .8b
+.DESCRIPTION
+    Attempts to Import all VMX file, on all Datastores, on a specific Datacenter
+.NOTES
+    Author(s): Christopher Thayer and Kevin Byrne
+    Requirements:  
+    Make sure the VMware PowerCLI is installed
+    https://developer.vmware.com/powercli/installation-guide
+#>
+
+$vCenter = "dpvttovctr01.shared.corp.nrt"                   # vCenter / vSphere your connecting to
+$datacenterName = "DenverTitleOneVDI"                       # Name of the virtual datacenter in vCenter (Determines datastores being scanned)
+$vmFolder = "Import Test"                                   # Folder to import VMs into
+$hostCluster = "VDI01"                                      # Cluster to register VMs on
+$OldNetworkPortGroup = "Data"                               # Old Network
+$NewNetworkPortGroup = "DVPG VLAN 1136 10.250.136.0/21"     # New Network
+$logfilePath = "C:\Powershell-Logs\vSphere-Import.txt"      # Path of Log File for Script
+
+Start-Transcript -Path "$logfilePath" -Append
+
+#Connect to vCenter
+try {
+    Write-Output "", "Connecting to vSphere Sever....."
+    Connect-VIServer -Server $vCenter
+    }
+catch {
+    Write-Output "", "---------------Not connected, cannot continue!---------------", ""
+    Stop-Transcript
+    exit -1
+    }
+
+# DEBUG Remove for Production
+$datastoreList = "Pure-VDI-Test"
+#Enable for Production
+###$datastoreList = (get-childitem -path "vmstores:\$vCenter@443\$datacenterName")
+Write-Output "", "The Following Datastores were found: ", $datastoreList
+
+foreach ($ds in $datastoreList) {
+    
+    #Checking The datastore for all VMX Files
+    Write-Output "", "*** Scanning $ds for VMX files...***", ""
+    $vmxList = Get-Childitem -Path "vmstores:\$vCenter@443\$datacenterName\$ds" -Recurse | where { $_.Name -like "*.vmx" }
+    
+    foreach ($vmConfigFile in $vmxList) {
+        
+        #Reset the loop and check each VMX to see if it has already been imported or exists
+        $vm = $null
+        $import = $false
+        $vmName = $vmConfigFile.name.replace(".vmx","")
+        Write-Output "====================================="
+        Write-Output "", "Checking if VM $vmName is found in vCenter..."
+        $vm = Get-VM $vmName -ErrorAction SilentlyContinue
+        
+        if (-not $vm) {
+            Write-Output "$vmName does not exist in vCenter.", ""
+            $import = $true
+        }
+        else {
+            Write-Output "$vmName is already in vCenter. SKIPPING!", ""
+            $import = $false
+        }
+        
+        # Import the VM if needed
+        if ($import) {
+            
+            # Get host with the most available memory (Make sure DRS is on
+            $targetHost = (Get-Cluster $hostCluster | Get-VMHost | Sort -Property MemoryUsageGB)[0]
+
+            # Register the VM
+            try {
+                New-VM -VMHost $targetHost -Name $vmName -Location $vmFolder -VMFilePath $($vmConfigFile.DatastoreFullPath) | Out-Null
+                #Change the Old Network to your current Portgroup
+                Get-VM $vmName |Get-NetworkAdapter |Where {$_.NetworkName -eq $OldNetworkPortGroup} |Set-NetworkAdapter -Portgroup $NewNetworkPortGroup -Confirm:$false
+                Write-Output "I have registered $vmName on the host $targetHost and updated the network to $NewNetworkPortGroup", ""
+                }
+            catch {
+                Write-Output "Import was not successful!", ""                               
+                }
+        }
+    }
+}
+
+Write-Output "", "*************** Importing Complete!***************", ""
+Disconnect-VIServer -Server $vCenter -Confirm:$false
+Stop-Transcript | Out-Null
